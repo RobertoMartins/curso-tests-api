051aaddc873f40543b1293718818e54a1bc57d2e
Roberto Murilo Martins Cordeiro <robertomurilocordeiro@gmail.com>
2022-10-04 23:32:36 -0300

    Initial commit

---
+++ src/main/java/com/robertomartins/api/CursoApiJunitTestApplication.java
@@ -0,0 +1,13 @@
+package com.robertomartins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiJunitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoApiJunitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robertomartins/api/CursoApiJunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertomartins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiJunitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
