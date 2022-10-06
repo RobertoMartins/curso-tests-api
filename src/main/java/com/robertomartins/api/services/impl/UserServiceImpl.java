84d183c31e806a6808840ec28b7db5b2976398e1
Roberto Murilo Martins Cordeiro <robertomurilocordeiro@gmail.com>
2022-10-06 00:45:26 -0300

    Create exception handling for object not found

--- src/main/java/com/robertomartins/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/robertomartins/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.robertomartins.api.services.impl;
 import com.robertomartins.api.domain.User;
 import com.robertomartins.api.repositories.UserRepository;
 import com.robertomartins.api.services.UserService;
+import com.robertomartins.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
