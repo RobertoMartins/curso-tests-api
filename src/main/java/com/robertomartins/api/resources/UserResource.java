04bade33a364900311dca7296cf9fa104d996dc7
Roberto Murilo Martins Cordeiro <robertomurilocordeiro@gmail.com>
2022-10-09 16:08:19 -0300

    Create findAll endpoint for User

--- src/main/java/com/robertomartins/api/resources/UserResource.java
+++ src/main/java/com/robertomartins/api/resources/UserResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -23,5 +26,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> listDTO = userService.findAll()
+                .stream().map(item -> mapper.map(item, UserDTO.class)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/robertomartins/api/services/UserService.java
+++ src/main/java/com/robertomartins/api/services/UserService.java
@@ -2,7 +2,10 @@ package com.robertomartins.api.services;
 
 import com.robertomartins.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
